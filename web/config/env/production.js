5c0a037d177cd1dc6aaa3d6022b25ef86e8ee345
Rodney <reln13st@gmail.com>
2016-03-06 19:22:20 +0100

    change to port 80

--- web/config/env/production.js
+++ web/config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 80,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
@@ -34,5 +34,7 @@ module.exports = {
   // log: {
   //   level: "silent"
   // }
+
+  hookTimeout: 40000
 
 };
