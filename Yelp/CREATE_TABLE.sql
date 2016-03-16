fbbf846885ddeb0c85917f89ea3c6eb4f950e8ca
mr3m <carlos@prosciana.com>
2016-03-16 14:36:06 +0100

    add create table sql

---
+++ Yelp/CREATE_TABLE.sql
@@ -0,0 +1,11 @@
+CREATE TABLE ip.yelp (
+	name varchar(500) NULL,
+	rating float4 NULL,
+	latitude float8 NULL,
+	longitude float8 NULL,
+	image_url varchar(500) NULL,
+	phone varchar(100) NULL,
+	review_count int8 NULL,
+	idd int8 NULL
+)
+;
