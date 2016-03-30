7a5639f3f2cc504527d6d47a9b99c6c1da8ed717
mr3m <carlos@prosciana.com>
2016-03-30 10:57:53 +0200

    Add DWH Proposal

--- EC2_tweet/config.sh
+++ EC2_tweet/config.sh
@@ -8,3 +8,7 @@ git clone https://github.com/DMKM1517/SmartCity.git
 nohup python tweet.py >> tweet.txt
 
 http://ec2-54-152-27-131.compute-1.amazonaws.com/dmkm/tweet.txt
+rsync -e "ssh -i Poincare.pem" -rca --progress ubuntu@50.16.139.89:tweet.txt tweet.txt
+http://ec2-50-16-139-89.compute-1.amazonaws.com/dmkm/tweet.txt
+http://50.16.139.89:1337/#/
+http://www.mr3m.me/smart
