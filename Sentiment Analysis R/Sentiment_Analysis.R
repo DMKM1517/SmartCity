3b1ae019e5f749ef9bc7e88b0343f201b5784294
mr3m <carlos@prosciana.com>
2016-04-11 23:41:57 +0200

    Connection and docs

--- Sentiment Analysis R/Sentiment_Analysis.R
+++ Sentiment Analysis R/Sentiment_Analysis.R
@@ -3,26 +3,29 @@
 #install.packages("dplyr")
 #install.packages("RPostgreSQL")
 #install.packages("SnowballC")
+install.packages("jsonlite")
+
 require("RPostgreSQL")
 require("stringi")
 library(gsubfn)
 library(SnowballC)
 library(dplyr)
-
+library(jsonlite)
 
 # ######### CONNECTION TO DB ###############
 
 
+login <- fromJSON("../login.json", flatten=TRUE)
 
 # loads the PostgreSQL driver
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(
-  drv, dbname = "smart",
-  host = "50.16.139.89",
-  port = 5432,
-  user = "dmkm", 
-  password = "dmkm1234"
-)
+  drv, dbname = login$dbname,
+   host = login$host,
+   port = login$port,
+   user = login$user,
+   password = login$password
+  )
 
 
 #query to get tweets
@@ -48,11 +51,11 @@ score.sentiment = function(idd, sentences, pos.words, neg.words, .progress='none
 {
   require(plyr)
   require(stringr)
-  
+
   # we got a vector of sentences. plyr will handle a list or a vector as an "l" for us
   # we want a simple array of scores back, so we use "l" + "a" + "ply" = laply:
   scores = laply(sentences, function(sentence, pos.words, neg.words) {
-    
+
     Encoding(sentence) <- "UTF-8"
     # clean up sentences with R's regex-driven global substitute, gsub():
     sentence = gsub('http\\S+\\s*', '', sentence)
@@ -61,40 +64,40 @@ score.sentiment = function(idd, sentences, pos.words, neg.words, .progress='none
     sentence = gsub('[[:cntrl:]]', '', sentence)
     sentence = gsub('\\d+', '', sentence)
     sentence = stri_trans_general(sentence ,"Latin-ASCII")
-    
+
     #Clean emojis
     sentence <- sapply(sentence, function(row) iconv(row, "latin1", "ASCII", sub=""))
     names(sentence) <- NULL
-    
+
     # replace al \n
     sentence <- gsub("[\n]", " ", sentence)
-    
+
     # convert to lower case:
     sentence = tolower(sentence)
-    
+
     # split into words. str_split is in the stringr package
     word.list = str_split(sentence, '\\s+')
     # sometimes a list() is one level of hierarchy too much
     words = unlist(word.list)
-    
+
     #Stemming
     words = wordStem(words, language = "french")
-    
+
     # compare our words to the dictionaries of positive & negative terms
     pos.matches = match(words, pos.words)
     neg.matches = match(words, neg.words)
-    
+
     # match() returns the position of the matched term or NA
     # we just want a TRUE/FALSE:
     pos.matches = !is.na(pos.matches)
     neg.matches = !is.na(neg.matches)
-    
+
     # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
     score = sum(pos.matches) - sum(neg.matches)
-    
+
     return(score)
   }, pos.words, neg.words, .progress=.progress )
-  
+
   scores.df = data.frame(id=idd, score=scores, text=sentences)
   return(scores.df)
 }
@@ -170,8 +173,3 @@ for (i in 1:length(sentiment.score$id)){
 
 
 dbDisconnect(con)
-
-
-
-
-
