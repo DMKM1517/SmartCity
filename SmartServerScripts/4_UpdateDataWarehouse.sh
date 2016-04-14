3cd3aacfcf638004fa66eb1a9c5d1283714a7d04
Jamp <jampmil@yahoo.com>
2016-04-14 14:46:22 +0200

    Script to run the DWH Update and small fix to script for retreiving sources

--- SmartServerScripts/4_UpdateDataWarehouse.sh
+++ SmartServerScripts/4_UpdateDataWarehouse.sh
@@ -1,3 +1,49 @@
 #!/bin/bash
 
-# TODO!
+#Move to the scripts folder
+cd "${0%/*}"
+
+
+echo "####################################################################################################" >> 4_UpdateDataWarehouse.log 2>&1
+
+#Start IPs
+now=$(date)
+echo "Starting Update of DWH - Interest Points: $now"
+echo "Starting Update of DWH - Interest Points: $now" >> 4_UpdateDataWarehouse.log 2>&1
+
+# Sync IPs
+../../kettle-data-integration/pan.sh -file="../SmartCity/ETL_Kettle_Editables/ETL_SmartDW_1_IP.ktr" -level=Basic >> 4_UpdateDataWarehouse.log 2>&1
+
+#End IPs
+now=$(date)
+echo "End Update of DWH - Interest Points: $now"
+echo "End Update of DWH - Interest Points: $now" >> 4_UpdateDataWarehouse.log 2>&1
+
+#Start Locations
+now=$(date)
+echo "Starting Update of DWH - Locations: $now"
+echo "Starting Update of DWH - Locations: $now" >> 4_UpdateDataWarehouse.log 2>&1
+
+# Sync Locations
+../../kettle-data-integration/pan.sh -file="../SmartCity/ETL_Kettle_Editables/ETL_SmartDW_2_Location.ktr" -level=Basic >> 4_UpdateDataWarehouse.log 2>&1
+
+#End Locations
+now=$(date)
+echo "End Update of DWH - Locations: $now"
+echo "End Update of DWH - Locations: $now" >> 4_UpdateDataWarehouse.log 2>&1
+
+
+#Start FactTable
+now=$(date)
+echo "Starting Update of DWH - Fact Table: $now"
+echo "Starting Update of DWH - Fact Table: $now" >> 4_UpdateDataWarehouse.log 2>&1
+
+# Sync FactTable
+../../kettle-data-integration/pan.sh -file="../SmartCity/ETL_Kettle_Editables/ETL_SmartDW_3_FactTable.ktr" -level=Basic >> 4_UpdateDataWarehouse.log 2>&1
+
+#End FactTable
+now=$(date)
+echo "End Update of DWH - FactTable: $now"
+echo "End Update of DWH - FactTable: $now" >> 4_UpdateDataWarehouse.log 2>&1
+
+echo "####################################################################################################" >> 4_UpdateDataWarehouse.log 2>&1
