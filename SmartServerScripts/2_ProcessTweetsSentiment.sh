aa677bb3b43470514359904d5f9a6d895299cc95
Jamp <jampmil@yahoo.com>
2016-04-27 09:49:55 +0200

    Server Scripts for Processing Sentiment and connecting to IP added

--- SmartServerScripts/2_ProcessTweetsSentiment.sh
+++ SmartServerScripts/2_ProcessTweetsSentiment.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-# TODO!
+#Move to the scripts folder
+cd "${0%/*}"
+
+
+echo "####################################################################################################" >> 2_ProcessTweetsSentiment.log 2>&1
+
+#Start Scoring Tweets
+now=$(date)
+echo "Starting Scoring Tweets: $now"
+echo "Starting Scoring Tweets: $now" >> 2_ProcessTweetsSentiment.log 2>&1
+
+# Score Tweets
+python ../DataScienceNotebooks/ScoreData.py >> 2_ProcessTweetsSentiment.log 2>&1
+
+#End Scoring Tweets
+now=$(date)
+echo "End Scoring Tweets: $now"
+echo "End Scoring Tweets: $now" >> 2_ProcessTweetsSentiment.log 2>&1
+
+echo "####################################################################################################" >> 2_ProcessTweetsSentiment.log 2>&1
--- SmartServerScripts/3_ConnectTweetsWithIP.sh
+++ SmartServerScripts/3_ConnectTweetsWithIP.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-# TODO!
+#Move to the scripts folder
+cd "${0%/*}"
+
+
+echo "####################################################################################################" >> 3_ConnectTweetsWithIP.log 2>&1
+
+#Start Connecting Tweets with IPs
+now=$(date)
+echo "Starting Connecting Tweets with IPs: $now"
+echo "Starting Connecting Tweets with IPs: $now" >> 3_ConnectTweetsWithIP.log 2>&1
+
+# Score Tweets
+Rscript ../TweetsToIP/FM_Keywords_Tweets_to_IP.R
+
+#End Connecting Tweets with IPs
+now=$(date)
+echo "End Connecting Tweets with IPs: $now"
+echo "End Connecting Tweets with IPs: $now" >> 3_ConnectTweetsWithIP.log 2>&1
+
+echo "####################################################################################################" >> 3_ConnectTweetsWithIP.log 2>&1
