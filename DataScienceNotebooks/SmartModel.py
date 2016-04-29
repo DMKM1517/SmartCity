453a52c9951a60ee4cb80708b15c36bceee215d8
Rodney <reln13st@gmail.com>
2016-04-29 08:57:33 +0200

    fix merge SmartModel.py

--- DataScienceNotebooks/SmartModel.py
+++ DataScienceNotebooks/SmartModel.py
@@ -15,14 +15,9 @@ def SmartModel(text):
     import pandas as pd
     import os
     from sklearn.externals import joblib
-<<<<<<< HEAD
-    clf = joblib.load('ModelObjects/modelrf2804.pkl') 
-    vectorizer = joblib.load('ModelObjects/tfidf.pkl') 
-=======
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ModelObjects')
     clf = joblib.load(os.path.join(path, 'svm.pkl')) 
     vectorizer = joblib.load(os.path.join(path, 'tfidf.pkl')) 
->>>>>>> 683d5671a44fc1b0cbb5c3dec0e1139bedede06e
     dataframe =  pd.Series([text])
     dataframe['text']= dataframe.apply(lambda x : processTweet(x))
     test_vectors = vectorizer.transform(dataframe['text'])
