4029c8cc191f0b8564ca697ea281d5973404ef24
Krishna Kalyan <krishnakalyan3@gmail.com>
2016-04-29 10:23:00 +0200

    Update ScoreData.py

--- DataScienceNotebooks/ScoreData.py
+++ DataScienceNotebooks/ScoreData.py
@@ -48,7 +48,7 @@ def processTweet(tweet):
 
 
 
-clf = joblib.load('/home/dmkm/SmartCity/DataScienceNotebooks/ModelObjects/modelrf2804.pkl') 
+clf = joblib.load('/home/dmkm/SmartCity/DataScienceNotebooks/ModelObjects/svm.pkl') 
 vectorizer = joblib.load('/home/dmkm/SmartCity/DataScienceNotebooks/ModelObjects/tfidf.pkl') 
 
 
