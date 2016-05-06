e2e47a12e27e473f451db07947108aa81c90c341
Katerina <kat.bastrakova@gmail.com>
2016-05-06 22:44:59 +0200

    Including BOW in cron script

--- SmartServerScripts/2_ProcessTweetsSentiment.sh
+++ SmartServerScripts/2_ProcessTweetsSentiment.sh
@@ -3,20 +3,35 @@
 #Move to the scripts folder
 cd "${0%/*}"
 
+echo "####################################################################################################" >> 2_ProcessTweetsSentiment.log 2>&1
+
+#Start Scoring Tweets BOW
+now=$(date)
+echo "Starting Scoring Tweets BOW: $now"
+echo "Starting Scoring Tweets BOW: $now" >> 2_ProcessTweetsSentiment.log 2>&1
+
+# Score Tweets BOW
+Rscript ../Sentiment\ Analysis\ R/Sentiment_Analysis.R >> 2_ProcessTweetsSentiment.log 2>&1
+
+#End Scoring Tweets BOW
+now=$(date)
+echo "End Scoring Tweets BOW: $now"
+echo "End Scoring Tweets BOW: $now" >> 2_ProcessTweetsSentiment.log 2>&1 
+
 
 echo "####################################################################################################" >> 2_ProcessTweetsSentiment.log 2>&1
 
-#Start Scoring Tweets
+#Start Scoring Tweets Python
 now=$(date)
-echo "Starting Scoring Tweets: $now"
-echo "Starting Scoring Tweets: $now" >> 2_ProcessTweetsSentiment.log 2>&1
+echo "Starting Scoring Tweets Python: $now"
+echo "Starting Scoring Tweets Python: $now" >> 2_ProcessTweetsSentiment.log 2>&1
 
-# Score Tweets
+# Score Tweets Python
 python ../DataScienceNotebooks/ScoreData.py >> 2_ProcessTweetsSentiment.log 2>&1
 
-#End Scoring Tweets
+#End Scoring Tweets Python
 now=$(date)
-echo "End Scoring Tweets: $now"
-echo "End Scoring Tweets: $now" >> 2_ProcessTweetsSentiment.log 2>&1
+echo "End Scoring Tweets Python: $now"
+echo "End Scoring Tweets Python: $now" >> 2_ProcessTweetsSentiment.log 2>&1
 
 echo "####################################################################################################" >> 2_ProcessTweetsSentiment.log 2>&1
